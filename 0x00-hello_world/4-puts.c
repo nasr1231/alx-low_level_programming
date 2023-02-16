1d257103e3b7aa0933c30d5d3310b6e4bd290212
Mohamed Nasr <nasr.1547474@gmail.com>
2023-02-16 15:40:55 -0800

    ha

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * return: Always 0
+* main - prints "Programming is like building a multilingual puzzel,"
+* Return: Always 0.
 */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
