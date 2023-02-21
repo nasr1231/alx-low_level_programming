997589f6bd3b401647283d8177e4152f9a27838b
Mohamed Nasr <nasr.1547474@gmail.com>
2023-02-21 15:03:04 -0800

    pushing task 0x02 -01

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program to print "_putchar" with putchar function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
